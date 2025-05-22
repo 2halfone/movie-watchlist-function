396bd8993c630474f2f19f7ec53091d692a5ff42
2halfone <2halfone@gmail.com>
2025-05-22 14:41:13 +0100

    Google Cloud Aggiungi spring-cloud-function-starter per FunctionCatalog

--- src/main/java/com/example/movie_watchlist_function/MovieWatchlistFunctionApplication.java
+++ src/main/java/com/example/movie_watchlist_function/MovieWatchlistFunctionApplication.java
@@ -3,13 +3,10 @@ package com.example.movie_watchlist_function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = ContextFunctionCatalogAutoConfiguration.class)
 @ComponentScan(basePackages = "com.example")
 @EntityScan("com.example")
 @EnableJpaRepositories("com.example")
